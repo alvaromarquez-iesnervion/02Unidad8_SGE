2e6f8fbe0b9f7285bffb74a1a105a06b3e2f0046
alvaromarquez-iesnervion <alvaro.marquez@iesnervion.es>
2025-11-03 14:25:50 +0100

    Agregar archivos de proyecto.

---
+++ 02Unidad8_SGE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _02Unidad8_SGE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ 02Unidad8_SGE/Models/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Unidad8_SGE.Models;
+
+public class Persona
+{   
+    // Propiedades públicas
+    public int Id { get; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellidos { get; set; } = string.Empty;
+
+    // Constructor vacío requerido
+    public Persona()
+    {
+       
+    }
+
+    // Constructor completo
+    public Persona(int id, string nombre, string apellidos)
+    {
+        Id = id;
+        Nombre = nombre;
+        Apellidos = apellidos;
+    }
+}
