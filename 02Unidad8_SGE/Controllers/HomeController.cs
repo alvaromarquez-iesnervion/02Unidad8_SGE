b6b0f6781f9c4977caee047484ec89a4478bb1e6
Alvaro Marquez Cobos <alvaro.marquez@iesnervion.es>
2025-11-04 16:35:23 +0100

    Ejercicios resueltos

--- 02Unidad8_SGE/Controllers/HomeController.cs
+++ 02Unidad8_SGE/Controllers/HomeController.cs
@@ -29,10 +29,39 @@ namespace _02Unidad8_SGE.Controllers
             return View();
         }
 
+        public IActionResult Ejercicio2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Ejercicio2(String nombre)
+        {
+            ViewBag.Nombre = nombre;
+            return View("Saludo");
+        }
+
+        public IActionResult Ejercicio3()
+        {   
+            Persona persona = new Persona(1, "Eryka", "Zapata Navarro");
+            return View(persona);
+        }
+
+        [HttpPost] 
+        public IActionResult Ejercicio3(Persona persona)
+        {
+            return View("PersonaModificada", persona);
+        }
+
+
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
